92ffcd68e0693c1b4c723600a76df130177f3ee5
sanashaik1998 <75576778+sanashaik1998@users.noreply.github.com>
2021-01-07 17:49:06 +0530

    Update LoginServiceImpl.java

--- LoginServiceImpl.java
+++ LoginServiceImpl.java
@@ -1,7 +1,6 @@
 package com.hcl.service;
 
-import org.apache.log4j.LogManager;
-import org.apache.log4j.Logger;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -9,25 +8,17 @@ import org.springframework.transaction.annotation.Transactional;
 import com.hcl.dao.LoginDao;
 import com.hcl.exception.ApplicationException;
 import com.hcl.model.User;
-/**
- * 
- * @author LakamsaniHanumanSai(51897508)
- *
- */
-
 @Service("loginServiceImpl")
 public class LoginServiceImpl implements LoginService {
 
 	@Autowired
 	private LoginDao loginDaoImpl;
 	
-	private static final Logger logger= LogManager.getLogger(LoginServiceImpl.class);
+
 	
 	@Override
 	@Transactional
-	public User validateUser(User user) throws ApplicationException {
-	
-		logger.info(user.getUserName()+" entered into validateUser method of LoginDao in service layer");
+	public User validateUser(User user) throws ApplicationException
 		return loginDaoImpl.validateUser(user);
 	}
 }
